eb64aabea663fbf6b99adb4ace9296ded64c5245
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-09-02 22:53:38 +0300

    Add a simple test

---
+++ tests/MPMCQueue.NET.Tests/RandomTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace MPMCQueue.NET.Tests
+{
+    public class RandomTests
+    {
+        private readonly MPMCQueue<int> _queue;
+
+        public RandomTests()
+        {
+            _queue = new MPMCQueue<int>(65536);
+        }
+
+        [Fact]
+        public void CanEnqueueAndDequeue()
+        {
+            _queue.TryEnqueue(1);
+            int actual;
+            _queue.TryDequeue(out actual);
+
+            Assert.Equal(1, actual);
+        }
+    }
+}
