f7491ea625281668cca6c1629f11770a677c3e6b
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-09-05 11:46:31 +0300

    Indeed random

--- tests/MPMCQueue.NET.Tests/RandomTests.cs
+++ tests/MPMCQueue.NET.Tests/RandomTests.cs
@@ -15,10 +15,17 @@ namespace MPMCQueue.NET.Tests
         public void CanEnqueueAndDequeue()
         {
             _queue.TryEnqueue(1);
+            _queue.TryEnqueue(2);
+            _queue.TryEnqueue(3);
             int actual;
             _queue.TryDequeue(out actual);
-
             Assert.Equal(1, actual);
+
+            _queue.TryDequeue(out actual);
+            Assert.Equal(2, actual);
+
+            _queue.TryDequeue(out actual);
+            Assert.Equal(3, actual);
         }
     }
 }
