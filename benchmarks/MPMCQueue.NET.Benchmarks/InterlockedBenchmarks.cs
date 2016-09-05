fdc62ab447d2444c4ac50217190e61bb636eaa9b
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-09-05 12:38:04 +0300

    Interlocked benchmark

---
+++ benchmarks/MPMCQueue.NET.Benchmarks/InterlockedBenchmarks.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using BenchmarkDotNet.Attributes;
+
+namespace MPMCQueue.NET.Benchmarks
+{
+    [Config(typeof(Config))]
+    public class InterlockedBenchmarks
+    {
+        private int _counter;
+
+        [Benchmark]
+        public void Increment()
+        {
+            Interlocked.Increment(ref _counter);
+        }
+
+        [Benchmark]
+        public void CAS()
+        {
+            Interlocked.CompareExchange(ref _counter, _counter + 1, _counter);
+        }
+    }
+
+}
