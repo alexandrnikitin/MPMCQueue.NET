ce8334bec58d02ad9fb48c6937140907d7866337
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-09-20 22:28:06 +0300

    Add SandboxApp

---
+++ sandbox/MPMCQueue.NET.SandboxApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace MPMCQueue.NET.SandboxApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Sandbox.V2.MPMCQueue<bool>(65536);
+            Wrapper.CanEnqueueAndDequeue(queue);
+            Console.ReadKey();
+
+        }
+    }
+
+    public class Wrapper
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void CanEnqueueAndDequeue(Sandbox.V2.MPMCQueue<bool> queue)
+        {
+            queue.TryEnqueue(true);
+        }
+    }
+
+}
