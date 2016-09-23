2e57eae11a8e027d6b89e784e386f62b95e176a6
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-09-23 14:32:28 +0300

    Add version with padding

---
+++ sandbox/MPMCQueue.NET.Sandbox/V4/MPMCQueue.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+using MPMCQueue.NET.Sandbox.V4.Padded.Fody;
+
+namespace MPMCQueue.NET.Sandbox.V4
+{
+    [Padded]
+    public class MPMCQueue<T>
+    {
+        private readonly Cell[] _buffer;
+        private readonly int _bufferMask;
+
+        private int _enqueuePos;
+        private int _dequeuePos;
+
+
+        public MPMCQueue(int bufferSize)
+        {
+            if (bufferSize < 2) throw new ArgumentException($"{nameof(bufferSize)} should be greater than 2");
+            if ((bufferSize & (bufferSize - 1)) != 0) throw new ArgumentException($"{nameof(bufferSize)} should be a power of 2");
+
+            _bufferMask = bufferSize - 1;
+            _buffer = new Cell[bufferSize];
+
+            for (var i = 0; i < bufferSize; i++)
+            {
+                _buffer[i].Sequence = i;
+            }
+
+            _enqueuePos = 0;
+            _dequeuePos = 0;
+        }
+
+        public bool TryEnqueue(T item)
+        {
+            do
+            {
+                var pos = _enqueuePos;
+                var index = pos & _bufferMask;
+                var cell = _buffer[index];
+                if (cell.Sequence - pos == 0 && Interlocked.CompareExchange(ref _enqueuePos, pos + 1, pos) == pos)
+                {
+                    cell.Element = item;
+                    cell.Sequence = pos + 1;
+                    _buffer[index] = cell;
+                    return true;
+                }
+
+                if (cell.Sequence - pos < 0)
+                {
+                    return false;
+                }
+            } while (true);
+        }
+
+        public bool TryDequeue(out T result)
+        {
+            do
+            {
+                var pos = _dequeuePos;
+                var index = pos & _bufferMask;
+                var cell = _buffer[index];
+                if (cell.Sequence - (pos + 1) == 0 && Interlocked.CompareExchange(ref _dequeuePos, pos + 1, pos) == pos)
+                {
+                    result = cell.Element;
+                    cell.Sequence = pos + _bufferMask + 1;
+                    _buffer[index] = cell;
+                    return true;
+                }
+
+                if (cell.Sequence - (pos + 1) < 0)
+                {
+                    result = default(T);
+                    return false;
+                }
+            } while (true);
+        }
+
+        private struct Cell
+        {
+            public volatile int Sequence;
+            public T Element;
+        }
+    }
+}
---
+++ sandbox/MPMCQueue.NET.Sandbox/V4/PaddedAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MPMCQueue.NET.Sandbox.V4
+{
+    namespace Padded.Fody
+    {
+        public sealed class PaddedAttribute : Attribute { }
+    }
+}
