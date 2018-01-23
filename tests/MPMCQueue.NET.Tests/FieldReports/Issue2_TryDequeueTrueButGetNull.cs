042dfc9a1b8f4fc5878e00fd84658d04ccf8a0c6
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2018-01-23 14:44:54 +0200

    Add a repro from issue #2

---
+++ tests/MPMCQueue.NET.Tests/FieldReports/Issue2_TryDequeueTrueButGetNull.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MPMCQueue.NET.Tests.FieldReports
+{
+    public class Issue2_TryDequeueTrueButGetNull
+    {
+        static MPMCQueue queue { get; } = new MPMCQueue(2);
+        static void Enqueue()
+        {
+            while (true) { queue.TryEnqueue(1); }
+        }
+
+        static void Dequeue()
+        {
+            while (true)
+            {
+                if (queue.TryDequeue(out object t) && t == null)
+                {
+                    throw new Exception("Dequeue null");
+                }
+            }
+        }
+
+        [Fact]
+        public void Test()
+        {
+            var t1 = Task.Run(() => Enqueue());
+            var t2 = Task.Run(() => Dequeue());
+            t2.Wait(TimeSpan.FromSeconds(10));
+        }
+    }
+}
